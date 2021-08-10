1f6537323d9be0c686e78bf2d69c91b62534d60d
rajeshkumargup <rajeshkumar.gupta@basware.com>
2021-08-10 10:39:47 +0530

    CK in

---
+++ 7. JavaScript/Script1.js
@@ -0,0 +1,22 @@
+let a = 5;
+function myfunction() 
+{
+    var aww = 3;
+    let b = 15;
+}
+
+console.log(a);
+myfunction();
+
+console.log(aww);
+////function x() {
+////    for (var i = 0; i < 5; i++) {
+////        function y(num) {
+////            setTimeout(function () {
+////                console.log(num++);
+////            }, num * 1000);
+////        }
+////        y(i);
+////    }
+////}
+////x();
